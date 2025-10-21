bcd422a03644b9ea8d3ef1e2df955ff1686cde4c
arthursouzaa <arthurjf.estudo@gmail.com>
2025-10-21 00:17:10 -0300

    commit inicial - financetrack-app

---
+++ src/rotas.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import ListagemMetas from './views/listagem-metas';
+// import ListagemCursos from './views/listagem-cursos';
+// import ListagemProfessores from './views/listagem-professores';
+// import ListagemAlunos from './views/listagem-alunos';
+// import ListagemCategorias from './views/listagem-categorias';
+// import ListagemAtividadesComplementares from './views/listagem-atividades-complementares';
+// import AcompanhamentoAtividadesComplementares from './views/acompanhamento-atividades-complementares';
+
+// import Login from './views/login';
+import CadastroMeta from './views/cadastro-metas';
+// import CadastroCurso from './views/cadastro-curso';
+// import CadastroProfessor from './views/cadastro-professor';
+// import CadastroAluno from './views/cadastro-aluno';
+// import CadastroCategoria from './views/cadastro-categoria';
+// import CadastroAtividadeComplementar from './views/cadastro-atividade-complementar';
+
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
+function Rotas(props) {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path='/login' element={<Login />} /> */}
+        <Route
+          path='/cadastro-metas/:idParam?'
+          element={<CadastroMeta />}
+        />
+        {/* <Route path='/cadastro-cursos/:idParam?' element={<CadastroCurso />} />
+        <Route
+          path='/cadastro-professores/:idParam?'
+          element={<CadastroProfessor />}
+        />
+        <Route path='/cadastro-alunos/:idParam?' element={<CadastroAluno />} />
+        <Route
+          path='/cadastro-categorias/:idParam?'
+          element={<CadastroCategoria />}
+        />
+        <Route
+          path='/cadastro-atividades-complementares/:idParam?'
+          element={<CadastroAtividadeComplementar />}
+        /> */}
+        <Route path='/listagem-metas' element={<ListagemMetas />} />
+        {/* <Route path='/listagem-cursos' element={<ListagemCursos />} />
+        <Route path='/listagem-professores' element={<ListagemProfessores />} />
+        <Route path='/listagem-alunos' element={<ListagemAlunos />} />
+        <Route path='/listagem-categorias' element={<ListagemCategorias />} />
+        <Route
+          path='/listagem-atividades-complementares'
+          element={<ListagemAtividadesComplementares />}
+        />
+        <Route
+          path='/acompanhamento-atividades-complementares'
+          element={<AcompanhamentoAtividadesComplementares />}
+        /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Rotas;
---
+++ src/views/cadastro-metas.js
@@ -0,0 +1,157 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import Stack from '@mui/material/Stack';
+import Card from '../components/card';
+import FormGroup from '../components/form-group';
+
+import { mensagemSucesso, mensagemErro } from '../components/toastr';
+
+//import '../custom.css';
+
+import axios from 'axios';
+import { BASE_URL } from '../config/axios';
+
+function CadastroMeta() {
+  const { idParam } = useParams();
+
+  const navigate = useNavigate();
+
+  const baseURL = `${BASE_URL}/meta`;
+
+  const [id, setId] = useState('');
+  const [nome, setNome] = useState('');
+  const [valor, setValor] = useState('');
+  const [dataAlvo, setDataAlvo] = useState('');
+  const [investimentoIni, setInvestimentoIni] = useState('');
+
+  const [dados, setDados] = React.useState([]);
+
+  function inicializar() {
+    if (idParam == null) {
+      setId('');
+      setNome('');
+      setValor('');
+      setDataAlvo('');
+      setInvestimentoIni('');
+    } else {
+      setId(dados.id);
+      setNome(dados.nome);
+      setValor(dados.valor);
+      setDataAlvo(dados.dataAlvo);
+      setInvestimentoIni(dados.investimentoIni);
+    }
+  }
+
+  async function salvar() {
+    let data = { id, nome, valor, dataAlvo, investimentoIni };
+    data = JSON.stringify(data);
+    if (idParam == null) {
+      await axios
+        .post(baseURL, data, {
+          headers: { 'Content-Type': 'application/json' },
+        })
+        .then(function (response) {
+          mensagemSucesso(`Meta ${nome} cadastrado com sucesso!`);
+          navigate(`/listagem-meta`);
+        })
+        .catch(function (error) {
+          mensagemErro(error.response.data);
+        });
+    } else {
+      await axios
+        .put(`${baseURL}/${idParam}`, data, {
+          headers: { 'Content-Type': 'application/json' },
+        })
+        .then(function (response) {
+          mensagemSucesso(`Meta ${nome} alterado com sucesso!`);
+          navigate(`/listagem-meta`);
+        })
+        .catch(function (error) {
+          mensagemErro(error.response.data);
+        });
+    }
+  }
+
+  async function buscar() {
+    await axios.get(`${baseURL}/${idParam}`).then((response) => {
+      setDados(response.data);
+    });
+    setId(dados.id);
+    setNome(dados.nome);
+    setValor(dados.valor);
+    setDataAlvo(dados.dataAlvo);
+    setInvestimentoIni(dados.investimentoIni);
+  }
+
+    return (
+    <div className='container'>
+      <Card title='Cadastro de Meta'>
+        <div className='row'>
+          <div className='col-lg-12'>
+            <div className='bs-component'>
+              <FormGroup label='Nome: *' htmlFor='inputNome'>
+                <input
+                  type='text'
+                  id='inputNome'
+                  value={nome}
+                  className='form-control'
+                  name='nome'
+                  onChange={(e) => setNome(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='Valor: *' htmlFor='inputValor'>
+                <input
+                  type='text'
+                  id='inputValor'
+                  value={valor}
+                  className='form-control'
+                  name='valor'
+                  onChange={(e) => setValor(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='DataAlvo: *' htmlFor='inputDataAlvo'>
+                <input
+                  type='date'
+                  id='inputDataAlvo'
+                  value={dataAlvo}
+                  className='form-control'
+                  name='dataAlvo'
+                  onChange={(e) => setDataAlvo(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='InvestimentoIni:' htmlFor='inputInvestimentoIni'>
+                <input
+                  type='text'
+                  id='inputInvestimentoIni'
+                  value={investimentoIni}
+                  className='form-control'
+                  name='investimentoIni'
+                  onChange={(e) => setInvestimentoIni(e.target.value)}
+                />
+              </FormGroup>
+              <Stack spacing={1} padding={1} direction='row'>
+                <button
+                  onClick={salvar}
+                  type='button'
+                  className='btn btn-success'
+                >
+                  Salvar
+                </button>
+                <button
+                  onClick={inicializar}
+                  type='button'
+                  className='btn btn-danger'
+                >
+                  Cancelar
+                </button>
+              </Stack>
+            </div>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+export default CadastroMeta;
